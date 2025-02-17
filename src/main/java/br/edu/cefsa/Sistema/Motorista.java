c7ffad93fe78f3f19f299dd1f5ce3dfa0ee2657c
KAUE DOS SANTOS ANDRADE <082220027@faculdade.cefsa.edu.br>
2025-02-17 20:47:52 -0300

    Modelagem motorista, aula 04

---
+++ src/main/java/br/edu/cefsa/Sistema/Motorista.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.cefsa.Sistema;
+
+/**
+ *
+ * @author 082220027
+ */
+public class Motorista {
+    
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    
+}
